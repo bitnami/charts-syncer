8eb0fc966ba28838e7b7c40de3c4b0fecc39fa9a
Tomas Pizarro Moreno <tpizarro@vmware.com>
2020-06-22 16:29:50 +0200

    Improve error messages

--- api/config.go
+++ api/config.go
@@ -9,16 +9,16 @@ import (
 // Validate validates the config file is correct
 func (c *Config) Validate() error {
 	if _, err := url.ParseRequestURI(c.Source.Repo.Url); err != nil {
-		return errors.Errorf("source repo URL should be a valid URL")
+		return errors.Errorf(`"source.repo.url" should be a valid URL`)
 	}
 	if _, err := url.ParseRequestURI(c.Target.Repo.Url); err != nil {
-		return errors.Errorf("target repo URL should be a valid URL")
+		return errors.Errorf(`"target.repo.url" should be a valid URL`)
 	}
 	if c.Target.ContainerRegistry == "" {
-		return errors.Errorf("container Registry cannot be empty")
+		return errors.Errorf(`"target.containerRegistry" cannot be empty`)
 	}
 	if c.Target.ContainerRepository == "" {
-		return errors.Errorf("container Repository cannot be empty")
+		return errors.Errorf(`"target.containerRepository" cannot be empty`)
 	}
 	return nil
 }
