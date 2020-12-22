cabf4c4a817d31549827595373ee82c934931c54
Joseda Rios <jotadrilo@users.noreply.github.com>
2020-12-22 11:11:22 +0100

    feat(client): Add local client impl (#79)

--- api/config.go
+++ api/config.go
@@ -11,8 +11,12 @@ func (c *Config) Validate() error {
 	if _, err := url.ParseRequestURI(c.Source.Repo.Url); err != nil {
 		return errors.Errorf(`"source.repo.url" should be a valid URL: %v`, err)
 	}
-	if _, err := url.ParseRequestURI(c.Target.Repo.Url); err != nil {
-		return errors.Errorf(`"target.repo.url" should be a valid URL: %v`, err)
+	switch k := c.Target.Repo.Kind; k {
+	case Kind_CHARTMUSEUM, Kind_HELM, Kind_HARBOR, Kind_OCI:
+		if _, err := url.ParseRequestURI(c.Target.Repo.Url); err != nil {
+			return errors.Errorf(`"target.repo.url" should be a valid URL: %v`, err)
+		}
+	case Kind_LOCAL:
 	}
 	if c.Target.ContainerRegistry == "" {
 		return errors.Errorf(`"target.containerRegistry" cannot be empty`)
