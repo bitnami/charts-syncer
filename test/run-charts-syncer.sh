cf8212a456d0de0eb1998c0bc1126ea17b599e4b
Tomas Pizarro Moreno <tpizarro@vmware.com>
2022-05-09 15:45:19 +0200

    chore: update testing helm version and charts-syncer flags

--- test/run-charts-syncer.sh
+++ test/run-charts-syncer.sh
@@ -10,4 +10,4 @@ ROOT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd)"
 ## Wait for chartmuseum service (Timeout in 30s)
 wait-for-port --state=inuse 8080
 
-/tmp/dist/charts-syncer --config ${ROOT_DIR}/test/test-config.yaml sync --from-date 2020-10-01
+/tmp/dist/charts-syncer --config ${ROOT_DIR}/test/test-config.yaml sync --latest-version-only
