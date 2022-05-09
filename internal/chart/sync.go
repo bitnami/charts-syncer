b0f01d591c230412ab97649796fa265aa877f231
Kay Yan <yankay@users.noreply.github.com>
2022-05-09 17:30:33 +0800

    ContainerRegistry (#153)

--- internal/chart/sync.go
+++ internal/chart/sync.go
@@ -14,6 +14,11 @@ import (
 // repo to the target repo
 func ChangeReferences(chartPath, name, version string, source *api.Source, target *api.Target) error {
 	// Update values*.yaml
+	if target.GetContainerRegistry() == "" && target.GetContainerRepository() == "" {
+		// Skip modify value.yaml and readme
+		return nil
+	}
+
 	for _, f := range []string{
 		path.Join(chartPath, ValuesFilename),
 		path.Join(chartPath, ValuesProductionFilename),
