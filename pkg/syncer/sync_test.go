251475d597cc366e811982f37d0aaa47c053998e
Alejandro Moreno <amorenoc@vmware.com>
2024-02-12 16:24:35 +0100

    Remove unused functions
    - getChartIndex is not used anywhere in the code

--- pkg/syncer/sync_test.go
+++ pkg/syncer/sync_test.go
@@ -1,41 +1,14 @@
 package syncer_test
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"path/filepath"
 	"reflect"
 	"testing"
 
-	"github.com/bitnami/charts-syncer/api"
-	"github.com/bitnami/charts-syncer/pkg/client/repo"
-	"github.com/bitnami/charts-syncer/pkg/client/repo/helmclassic"
 	"github.com/bitnami/charts-syncer/pkg/syncer"
 )
-
-func getChartIndex(t *testing.T, name string, targetRepo *api.Target, tester repo.ClientTester) []*helmclassic.ChartVersion {
-	// Check the chart really was added to the service's index.
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/charts/%s", tester.GetURL(), name), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(targetRepo.GetRepo().GetAuth().GetUsername(), targetRepo.GetRepo().GetAuth().GetPassword())
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	charts := []*helmclassic.ChartVersion{}
-	if err := json.NewDecoder(resp.Body).Decode(&charts); err != nil {
-		t.Fatal(err)
-	}
-	return charts
-}
 
 func TestFakeSyncPendingCharts(t *testing.T) {
 	testCases := []struct {
